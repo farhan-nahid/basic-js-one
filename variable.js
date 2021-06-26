3abb6bda656c4935de62aec918a33fa57b4e2199
farhan-nahid <nahidahmed9933@gmail.com>
2021-06-26 19:46:02 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,7 @@
+const myName = "Farhan Ahmed Nahid";
+const age = 20;
+
+console.log(myName, age);
+console.log("My name is" + " " + myName);
+console.log("My age is" + " " + age);
+console.log(myName + " " + "loves JavaScript");
